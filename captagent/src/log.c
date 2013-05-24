f8387a94327e62e554c65ad6f7634f9906b206ed
Alexandr.Dubovikov <Alexandr.Dubovikov@gmail.com>
2013-05-24 12:29:52 +0000

    some changed for logging system

--- captagent/src/log.c
+++ captagent/src/log.c
@@ -1,6 +1,32 @@
+/*
+ * $Id$
+ *
+ *  captagent - Homer capture agent. Modular
+ *  Duplicate SIP messages in Homer Encapulate Protocol [HEP] [ipv6 version]
+ *
+ *  Author: Alexandr Dubovikov <alexandr.dubovikov@gmail.com>
+ *  (C) Homer Project 2012 (http://www.sipcapture.org)
+ *
+ * Homer capture agent is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version
+ *
+ * Homer capture agent is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+*/
+
 #include "log.h"
 #include <stdio.h>
 #include <stdarg.h>
+
 
 static int use_syslog = 0;
 static int log_level = LOG_WARNING;
@@ -12,27 +38,30 @@ void init_log(char *_prgname, int _use_syslog) {
         }
 }
 
-void set_log_level(int level) {
+void set_log_level(int level) {   
         log_level = level;
 }
+
 
 void destroy_log(void) {
         if (use_syslog) closelog();
 }
-
+  
+ 
 void log_stdout(char * format, va_list ap)
 {
         vfprintf(stdout, format, ap);
         fflush(stdout);
 }
+   
+void data_log(int priority, const char *fmt, ...) {
 
-void capt_log(int priority, char * format, ...) {
-        va_list ap;
-        if (priority<=log_level) {
-                va_start(ap, format);
-                if (use_syslog) vsyslog(priority, format, ap);
-                else log_stdout(format, ap);
-                va_end(ap);
+        va_list args;
+        if (priority<=log_level) {                
+                va_start(args, fmt);
+                if (use_syslog) vsyslog(priority, fmt, args);
+                else log_stdout(fmt, args);
+                va_end(args);
+                
         }
 }
-
