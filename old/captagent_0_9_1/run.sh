726785f854936658af7387acd7293d6bafe4dbff
Alexandr.Dubovikov <Alexandr.Dubovikov@gmail.com>
2014-04-18 18:23:41 +0000

    added some compile information for Solaris

--- old/captagent_0_9_1/run.sh
+++ old/captagent_0_9_1/run.sh
@@ -5,4 +5,16 @@ cc -o captagent captagent.c -lpcap
 #-lsocket
 
 #Solaris. Please be sure that your compiler is gcc or understand the packet attribute for structure
-#cc -o captagent captagent.c -lpcap -lsocket
+#cc -o captagent captagent.c -lpcap -lsocket -lnsl
+
+#Solaris 2.6 - 7
+#cc -o captagent captagent.c -lpcap -lsocket -lnsl -lresolv
+
+#If your libpcap is not in standart path. 
+# LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/my_path_to_libpcap
+# export LD_LIBRARY_PATH
+
+#or add the path to /etc/ld.so.conf and run ldconfig
+
+
+
