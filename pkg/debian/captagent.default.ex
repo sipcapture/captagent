3ccd12e82e61efd3b99df810d063e8abaf86b4c6
Konstantin S. Vishnivetsky <kvishnivetsky@sipcapture.org>
2015-09-17 14:05:41 +0600

    Add: basic DEB packaging support

---
+++ pkg/debian/captagent.default.ex
@@ -0,0 +1,10 @@
+# Defaults for captagent initscript
+# sourced by /etc/init.d/captagent
+# installed at /etc/default/captagent by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ pkg/debian/menu.ex
@@ -0,0 +1,2 @@
+?package(captagent):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="captagent" command="/usr/bin/captagent"
