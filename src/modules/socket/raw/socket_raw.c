bac9bd0950bd45b4e66e926b8f86085829251f1f
Alexandr Dubovikov <alexandr.dubovikov@gmail.com>
2015-12-07 15:23:15 +0100

    Update socket_raw.c

--- src/modules/socket/raw/socket_raw.c
+++ src/modules/socket/raw/socket_raw.c
@@ -605,7 +605,7 @@ static int load_module(xml_node *config) {
 
 					if (!strncmp(key, "dev", 3))
 						profile_socket[profile_size].device = strdup(value);
-					else if (!strncmp(key, "promisc", 7) && !strncmp(value, "false", 5))
+					else if (!strncmp(key, "promisc", 7) && !strncmp(value, "true", 4))
 						profile_socket[profile_size].promisc = 1;
 					else if (!strncmp(key, "filter", 6))
 						profile_socket[profile_size].filter = strdup(value);
