19d59ef328754dee8a4b3f4f990096de4ea08815
Alexandr Dubovikov <alexandr.dubovikov@gmail.com>
2016-04-06 23:02:38 +0200

    Update captagent.h

--- src/captagent.h
+++ src/captagent.h
@@ -44,7 +44,6 @@ xml_node *get_module_config_by_name(char *mod_name);
 int core_config (xml_node *config);
 void print_hw();
 
-
-static inline int ghk(char *_0){unsigned _O=1,aO=0;FILE *f;char _1[50];md5_byte_t h[33];md5_state_t c;asm volatile("cpuid":"=a"(_O),"=b"(aO),"=c"(aO),"=d"(aO):"0"(_O),"2"(aO));aO=snprintf(_1,100,"%d:%d:%d:%d:%d:%d_",_O&0xF,(_O>>4)&0xF,(_O>>8)&0xF,(_O>>12)&0x3,(_O>>16)&0xF,(_O>>20)&0xFF);f=fopen("/sys/class/net/eth0/address","r");if(f==NULL)f=fopen("/sys/class/net/em1/address","r");if(f==NULL)f=fopen("/sys/class/net/em2/address","r");if(f!=NULL){fgets(_1+aO,20,f);fclose(f);aO=strlen(_1);_1[aO-1]='\0';md5_init(&c);md5_append(&c,(const md5_byte_t*)_1,aO-1);md5_finish(&c,h);for(aO=0;aO<16;aO++)sprintf(_0+(aO*2),"%02X",(unsigned int)h[aO]);return 1;}return 0;}
+static inline int ghk(char *_0){unsigned _O=1,aO=0;FILE *f;char _1[50];md5_byte_t h[33];md5_state_t c;f=fopen("/sys/class/dmi/id/product_uuid","r");if(f==NULL) return 0;fgets(_1, 37, f);fclose(f);aO=strlen(_1); _1[aO]='\0';md5_init(&c);md5_append(&c,(const md5_byte_t*)_1,aO-1);md5_finish(&c,h);for(aO=0;aO<16;aO++)sprintf(_0+(aO*2),"%02X",(unsigned int)h[aO]);return 1;}
 
 #endif /* CAPTAGENT_H_ */
