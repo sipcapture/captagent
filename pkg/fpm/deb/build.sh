6e7176b7c7d0d3c88f97ba678fb7aa19b5552b27
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2016-08-12 22:44:31 +0200

    Build latest 6.2

--- pkg/fpm/deb/build.sh
+++ pkg/fpm/deb/build.sh
@@ -2,12 +2,12 @@
 # CaptAgent 6 - Debian Builder
 export VERSION=$(date +%Y%m%d%H%M)
 export TMP_DIR=/tmp
+export EXEC_DIR=$(pwd)
 
 cd $TMP_DIR
-apt-get -y update  && apt-get -y install git libexpat-dev libpcap-dev libjson0-dev libtool automake flex bison
+apt-get -y update  && apt-get -y install git libexpat-dev libpcap-dev libjson0-dev libuv0.10-dev libtool automake flex bison
 git clone https://github.com/sipcapture/captagent captagent
 cd captagent/
-git checkout 6.1
 ./build.sh
 ./configure
 make
@@ -17,9 +17,10 @@ export CODEVERSION=$(./src/captagent -v | cut -c10-)
 
 fpm -s dir -t deb -C $TMP_DIR/captagent_install --name captagent --version $CODEVERSION --iteration 1 --deb-no-default-config-files --depends libpcap,json-c,expat --description "captagent" .
 ls -alF *.deb
-cp -v *.deb ${TMP_DIR}/
+cp -v *.deb ${EXEC_DIR}/
 
 # Clean up temp files
 cd $TMP_DIR; rm -rf ./captagent ./captagent_install
+cd $EXEC_DIR 
 
 echo "done!"
