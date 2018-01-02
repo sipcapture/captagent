a650ffd77f521b67faf019e07f577c94e9089828
Campus <campus@ntop.org>
2018-01-02 13:54:07 +0100

    fix wrong return check value in diameter dissector

--- src/modules/protocol/diameter/parser_diameter.c
+++ src/modules/protocol/diameter/parser_diameter.c
@@ -36,9 +36,6 @@ int is_diameter(char *packet, int size_payload)
   // check param
   if(!packet || size_payload == 0) return -1;
 
-  // pointer used to move through the pkt
-  char *pp = packet;
-
   // cast to diameter header
   struct diameter_header_t *diameter = (struct diameter_header_t *) packet;
 
@@ -68,7 +65,7 @@ int parse_diameter(char *packet, int size_payload, char json_buffer[], int buffe
   int offset = 0, js_ret = 0;
   
   int ret = is_diameter(packet, size_payload);
-  if(ret != 1) return -1; // invalid params
+  if(ret != 0) return -1; // invalid params
 
   // cast to diameter header
   struct diameter_header_t *diameter = (struct diameter_header_t *) packet;
