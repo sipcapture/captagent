7343fda54908cafc6f23a8e00ee059a5f93c6edb
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2018-01-02 21:23:07 +0100

    Update build.sh

--- pkg/fpm/rpm/build.sh
+++ pkg/fpm/rpm/build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-# CaptAgent 6 - CentOS Builder
+# CaptAgent 6 - CentOS Builder for Docker
 export VERSION=$(date +%Y%m%d%H%M)
 export TMP_DIR=/tmp
+export EXEC_DIR=$(pwd)
 
 cd $TMP_DIR
 yum -y install json-c-devel expat-devel libpcap-devel flex-devel automake libtool bison
 git clone https://github.com/sipcapture/captagent captagent
 cd captagent/
-git checkout 6.1
 ./build.sh
 ./configure
 make
@@ -18,7 +18,7 @@ export CODEVERSION=$(./src/captagent -v | cut -c10-)
 fpm -s dir -t rpm -C $TMP_DIR/captagent_install --name captagent --version $CODEVERSION --iteration 1 --depends json-c,expat,libpcap --description "captagent" .
 
 ls -alF *.rpm
-cp -v *.rpm ${TMP_DIR}
+cp -v *.rpm ${EXEC_DIR}
 
 cd $TMP_DIR; rm -rf ./captagent ./captagent-installer
 
