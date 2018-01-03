f0e056348ccf43af1a495d3fd432836b9bb55443
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2018-01-03 11:47:20 +0100

    update rpm docker builders

--- pkg/fpm/rpm/build_tls.sh
+++ pkg/fpm/rpm/build_tls.sh
@@ -6,17 +6,17 @@ export EXEC_DIR=$(pwd)
 
 cd $TMP_DIR
 yum -y install epel-release
-yum -y install json-c-devel expat-devel libpcap-devel flex-devel automake libtool bison libuv-devel openssl-devel libgcrypt-devel
+yum -y install json-c-devel expat-devel libpcap-devel flex-devel automake libtool bison libuv-devel openssl-devel libgcrypt-devel libgcrypt
 git clone https://github.com/sipcapture/captagent captagent
 cd captagent/
 ./build.sh
-./configure
+./configure --enable-ssl --enable-tls
 make
 mkdir -p $TMP_DIR/captagent
 make DESTDIR=$TMP_DIR/captagent_install install
 export CODEVERSION=$(./src/captagent -v | cut -c10-)
 
-fpm -s dir -t rpm -C $TMP_DIR/captagent_install --name captagent --version $CODEVERSION --iteration 1 --depends json-c,expat,libpcap --description "captagent" .
+fpm -s dir -t rpm -C $TMP_DIR/captagent_install --name captagent --version $CODEVERSION --iteration 1 --depends json-c,expat,libpcap,libuv,openssl,libgcrypt --description "captagent" .
 
 ls -alF *.rpm
 cp -v *.rpm /scripts/
