c943d88680f441d5e88749c7e1234d16ade7784b
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2018-03-02 23:32:34 +0100

    baseline ci test

---
+++ test/index.js
@@ -0,0 +1,52 @@
+const assert = require('assert')
+const spawn = require('child_process').spawn
+
+/*
+ * Start Captagent, Check Exit Code
+ */
+describe('CaptAgent', () => {
+  let args = [
+    "-v"
+  ]
+  let exitCode
+
+  before((done) => {
+    let process = spawn('../src/captagent', args)
+    process.on('exit', (code) => {
+      exitCode = code
+      done()
+    })
+  })
+  it('exit code should be zero', () => {
+    assert.equal(exitCode, 0)
+  })
+ })
+
+/*
+ * Start Captagent, Check Exit Code
+ */
+describe('CaptAgent Version', () => {
+  let args = [
+    "-v"
+  ]
+  let exitCode
+  let output
+
+  before((done) => {
+    let process = spawn('../src/captagent', args)
+    process.stdout.on('data', function(data) {
+      output = data.toString();
+    });
+    process.on('exit', (code) => {
+      exitCode = code
+      done()
+    })
+  })
+  it('exit code should be zero', () => {
+    assert.equal(exitCode, 0)
+  })
+  it('should print version number', () => {
+    assert.ok(output.length > 0);
+    assert.ok(output.startsWith('version'));
+  })
+ })
