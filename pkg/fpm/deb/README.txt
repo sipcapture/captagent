038afbb78eb91786cc6772ab9eb72a7522a9e209
kyrol <fci1908@gmail.com>
2019-05-15 11:00:19 +0200

    UPD: deb pkg script update for captagent

--- pkg/fpm/deb/README.txt
+++ pkg/fpm/deb/README.txt
@@ -1,6 +1,8 @@
-# Syntax
+## The next commands are putted in run.sh and run_tls.sh 
 
+# run.sh
 docker run --rm -v $(pwd)/:/tmp/build -v $(pwd)/:/scripts --entrypoint=/scripts/build.sh  alanfranz/fwd-debian-jessie:latest
 
+# run_tls.sh
 docker run --rm -v $(pwd)/:/tmp/build -v $(pwd)/:/scripts --entrypoint=/scripts/build_tls.sh alanfranz/fwd-debian-jessie:latest
 
--- pkg/fpm/deb/build.sh
+++ pkg/fpm/deb/build.sh
@@ -1,26 +1,45 @@
 #!/bin/bash
-# CaptAgent 6 - Debian Builder
-export VERSION=$(date +%Y%m%d%H%M)
-export TMP_DIR=/tmp
-export EXEC_DIR=$(pwd)
+# CaptAgent 6 - Debian 9 Builder
 
-cd $TMP_DIR
-apt-get -y update  && apt-get -y install git libexpat-dev libpcap-dev libjson0-dev libuv0.10-dev libtool automake flex bison
+INV=1
+OS="stretch"
+VERSION_MAJOR="6.4"
+VERSION_MINOR="0"
+
+# install libs
+cd /tmp
+apt-get -y update  && apt-get -y install git make gcc libexpat-dev libpcap-dev libjson-c-dev libuv1-dev libtool automake autoconf flex bison
+
+# clone captagent and build it
 git clone https://github.com/sipcapture/captagent captagent
 cd captagent/
 ./build.sh
 ./configure
 make
-mkdir -p $TMP_DIR/captagent
-make DESTDIR=$TMP_DIR/captagent_install install
+
+# version
+mkdir -p /tmp/captagent
+make DESTDIR=/tmp/captagent_install install
 export CODEVERSION=$(./src/captagent -v | cut -c10-)
 
-fpm -s dir -t deb -C $TMP_DIR/captagent_install --name captagent --version $CODEVERSION --iteration 1 --deb-no-default-config-files --depends libpcap,json-c,expat --description "captagent" .
+# install ruby and fpm
+apt-get -y install ruby-dev rubygems build-essential
+gem install rake
+gem install --no-ri --no-rdoc fpm
+
+#DEPENDECY
+DEPENDENCY=`dpkg -l | grep -E "libexpat|libpcap|libjson-c|libuv" | grep -v "dev" | awk '{print $2}' | sed -e 's/:amd64//g' | tr '\n' ','`
+DEPENDENCY=${DEPENDENCY%?}; #Remove last characters
+echo $DEPENDENCY;
+
+# create deb pkg with fpm
+fpm -s dir -t deb -C /tmp/captagent_install --name captagent --version $CODEVERSION --iteration 1 \
+    -p "captagent_${VERSION_MAJOR}.${VERSION_MINOR}-${INV}.${OS}.amd64.deb" \
+    --deb-no-default-config-files --depends ${DEPENDENCY} --description "captagent" .
 ls -alF *.deb
 cp -v *.deb /scripts/
 
-# Clean up temp files
-cd $TMP_DIR; rm -rf ./captagent ./captagent_install
-cd $EXEC_DIR 
+# clean up temp files
+cd /tmp; rm -rf ./captagent ./captagent_install
 
 echo "done!"
