0d65b7a3b31e504630bb7fc8a82bc0d9bb6a615c
kyrol <fci1908@gmail.com>
2019-11-29 10:32:57 +0100

    FIX: fixing ci test

--- test/init.js
+++ test/init.js
@@ -4,27 +4,6 @@ const spawn = require('child_process').spawn
 /*
  * Start Captagent, Check Exit Code
  */
-describe('CaptAgent Initialization', () => {
-  let args = [
-    "-v"
-  ]
-  let exitCode
-
-  before((done) => {
-    let process = spawn('../src/captagent', args)
-    process.on('exit', (code) => {
-      exitCode = code
-      done()
-    })
-  })
-  it('exit code should be zero', () => {
-    assert.equal(exitCode, 0)
-  })
- })
-
-/*
- * Start Captagent, Check Exit Code
- */
 describe('CaptAgent Version', () => {
   let args = [
     "-v"
@@ -48,5 +27,33 @@ describe('CaptAgent Version', () => {
   it('should print version number', () => {
     assert.ok(output.length > 0);
     assert.ok(output.startsWith('version'));
+  })
+ })
+
+/*
+ * Start Captagent, CHeck list of devices and exit
+ */
+describe('CaptAgent Initialization', () => {
+  let args = [
+    "-a"
+  ]
+    let exitCode
+    let output
+
+  before((done) => {
+    process.stdout.on('data', function(data) {                                                                                                        
+      output = data.toString();                                                                                                                       
+    });                                                                                                                                               
+    process.on('exit', (code) => {                                                                                                                    
+      exitCode = code                                                                                                                                 
+      done()                                                                                                                                          
+    })
+  })
+  it('exit code should be zero', () => {
+    assert.equal(exitCode, 0)
+  })
+    it('should print the list of devices', () => {
+    assert.ok(output.length > 0);
+    assert.ok(output.startsWith(''));
   })
  })
